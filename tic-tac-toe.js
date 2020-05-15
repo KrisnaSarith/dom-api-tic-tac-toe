4e6462a40fcbfb3f6e769fa61ccc868144b4d1de
Krisna Sarith <krisnasarith@gmail.com>
2020-05-15 14:55:46 -0700

    phase 5

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -21,12 +21,15 @@ let tieGame = false;
 let header = document.getElementById("game-status");
 
 let newGameButton = document.getElementById("new-game");
-console.log("newGameButton is " + newGameButton.id);
+let giveUpButton = document.getElementById("give-up");
+//console.log("newGameButton is " + newGameButton.id);
 
 function isWinner (bothArrays, player){
     for( let i = 0; i < combos.length ; i++){
         if(bothArrays[player].includes(combos[i][0])&&bothArrays[player].includes(combos[i][1])&&bothArrays[player].includes(combos[i][2])){
             haveWinner = true;
+            newGameButton.removeAttribute("disabled");
+            giveUpButton.setAttribute("disabled", "");
             header.innerText = "Winner: "+ (player===0 ? "X" : "O");
             // alert("Winner!");
             return true;
@@ -34,27 +37,33 @@ function isWinner (bothArrays, player){
     }
     return false;
 }
-
-function resetGame (play) {
+let play;
+function resetGame () {
   play = 0;
-  boardArray = new Array(9);
-  xArray = [];
-  oArray = [];
+  player = 0;
+  boardArray = new Array(9).fill(undefined);
+  bothArrays[0] = [];
+  bothArrays[1] = [];
   haveWinner = false;
   tieGame = false;
   header.innerText = "";
+  newGameButton.setAttribute("disabled", "");
+  giveUpButton.removeAttribute("disabled");
   for (let i = 0; i < boardArray.length; i++) {
       document.getElementById(`square-${i}`).innerHTML = "";
   }
 }
 
+let player = 0;
 window.addEventListener("DOMContentLoaded", event =>{
-    let play = 0;
+    play = 0;
+
     document.getElementById("tic-tac-toe-board").addEventListener("click", event =>{
-        console.log(`This is the target ${event.target.id}`);
+        // console.log(`This is the target ${event.target.id}`);
+        // player = play%2;
     if(!haveWinner){
     //   console.log("Do we have a winner? " + haveWinner)
-      let player = play%2;
+
       let squareId = Number(event.target.id.slice(-1));
     //   console.log("squareID = " + squareId);
     //   console.log(boardArray[0]);
@@ -65,20 +74,41 @@ window.addEventListener("DOMContentLoaded", event =>{
         boardArray[squareId] = true;
         // console.log("squareID = " + squareId);
         let square = document.getElementById(`square-${squareId}`);
-        console.log(square);
+        // console.log(square);
         square.innerHTML = XO[player];
+        // console.log(bothArrays);
         isWinner(bothArrays, player);
         play++;
-        if(play===9){
-            header.innerText= "Winner: none!";
+        player= play%2;
+        if(play===9&&!haveWinner){
+            tieGame = true;
+            header.innerText = "Winner: none!";
+            newGameButton.removeAttribute("disabled");
+            giveUpButton.setAttribute("disabled","");
         }
       } else{
             // console.log("illegal move");
             alert('Illegal move, try again.');
       }
+      if (haveWinner|| tieGame){
+          newGameButton.removeAttribute("disabled");
+      }
     }
     });
     newGameButton.addEventListener("click", () => {
-      resetGame(play);
+        resetGame();
+    })
+    giveUpButton.addEventListener("click", () => {
+    //   console.log(player);
+    console.log(`Play: ${play}  Player: ${player}`);
+      header.innerText = "Winner: "+ (player===0 ? "O" : "X");
+      newGameButton.removeAttribute("disabled");
+      giveUpButton.setAttribute("disabled", "");
+      haveWinner = true;
+    //   if (player === 0) {
+    //       isWinner(bothArrays, 1);
+    //   } else {
+    //       isWinner(bothArrays, 0);
+    //   }
     })
 });
